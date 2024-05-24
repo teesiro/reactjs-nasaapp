983cd6472d10921030bcc8b946af742f1787b7f7
sarah inoniyegha <sarahalfred091@gmail.com>
2024-05-24 14:24:04 +0100

    used nasa API to create a nasa react app

---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main(props) {
+    const { data } = props
+    return (
+        <div className="imageContainer">
+            <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />
+     </div>    
+  )
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+export default function Sidebar(props) {
+    const { handleToggleModal, data } = props
+    return (
+        <div onClick={handleToggleModal} className="sidebar">
+            <div onClick={handleToggleModal} className="bgOverlay"></div>
+            <div className="sidebarContent">
+                <h2>{data?.title}</h2>
+                <div className="descriptionContainer">
+                    <p className="descriptionTitle">{data?.date}</p>
+                    <p>{data?.explanation}</p>
+                </div> 
+                <button onClick={handleToggleModal}>
+                    <i className="fa-solid fa-arrow-right"></i>
+                </button>    
+            </div>
+        </div>
+    )
+}
